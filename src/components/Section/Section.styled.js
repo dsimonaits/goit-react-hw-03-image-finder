bc3bdad2b78a2a0200d94b6d632d8770ffd97ba5
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-05 16:09:44 +0200

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,12 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  border: ${props => (props.border === 'true' ? '1px solid black' : 'none')};
-  width: 500px;
-  /* max-height: 500px; */
   height: auto;
   padding: 5px;
-
-  text-align: center;
   margin: 0 auto;
 `;
