40a96191866f463a19512ada0b44706a5afe3e19
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-05 16:04:55 +0200

    Create pixabay-api.js

---
+++ src/services/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const API_KEY = '30781043-697065eb175f1ee93924e4241';
+
+async function fetchImages(query, page) {
+  try {
+    const response = await axios({
+      params: {
+        key: API_KEY,
+        q: query,
+        page: page,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: '12',
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default fetchImages;
