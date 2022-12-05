8839fb8a68de2c11516011633764aa74cefddd2c
Deniss Simonaits <dsimonaits@gmail.com>
2022-12-05 16:09:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,111 @@
 import React, { Component } from 'react';
-import Section from './Section';
-import { nanoid } from 'nanoid';
+import Searchbar from './Searchbar';
+import fetchImages from 'services/pixabay-api';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
+import Modal from './Modal/Modal';
+import { AppStyled } from './App.styled';
+import { Container } from './Section/Section.styled';
+import { InfinitySpin } from 'react-loader-spinner';
 
-class App extends Component {
-  state = {};
+export default class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    status: 'idle',
+    currentImage: null,
+    isLoading: false,
+    error: '',
+  };
 
-  componentDidMount() {}
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
 
-  componentDidUpdate(prevProps, prevState) {}
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImages();
+    }
+  }
 
-  componentWillUnmount() {}
+  getImages = () => {
+    const { query, page } = this.state;
+    this.setState({ status: 'pending' });
+    fetchImages(query, page)
+      .then(({ data: { hits } }) => {
+        if (hits.length !== 0) {
+          this.setState(prevState => ({
+            images: [...prevState.images, ...hits],
+            status: 'resolved',
+            error: '',
+          }));
+        } else {
+          this.setState({ status: 'rejected' });
+        }
+      })
+      .catch(error => {
+        this.setState({
+          error: error.message,
+        });
+      })
+      .finally(() => {
+        this.setState({
+          isLoading: false,
+        });
+      });
+  };
+
+  handleFormSubmit = query => {
+    if (query.trim() !== '') {
+      this.setState({ query, page: 1, images: [] });
+    } else {
+      this.setState({ status: 'emptyQuery' });
+    }
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  openModal = data => {
+    this.setState({ currentImage: data });
+  };
+
+  closeModal = () => {
+    this.setState({ currentImage: null });
+  };
 
   render() {
+    const { images, status, currentImage } = this.state;
     return (
-      <>
-        <Section title="Phonebook" border="false"></Section>
-      </>
+      <AppStyled>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {status === 'resolved' && (
+          <>
+            <ImageGallery images={images} openModal={this.openModal} />
+            <Button text="Load more" clickHandler={this.loadMore} />
+            {currentImage && (
+              <Modal image={currentImage} closeModal={this.closeModal} />
+            )}
+          </>
+        )}
+        {status === 'rejected' && (
+          <Container>
+            <p>There is no search matches!</p>
+          </Container>
+        )}
+        {status === 'emptyQuery' && (
+          <Container>
+            <p>Write something first!</p>
+          </Container>
+        )}
+        {status === 'pending' && (
+          <Container>
+            <InfinitySpin width="200" color="#3f51b5" />
+          </Container>
+        )}
+      </AppStyled>
     );
   }
 }
-
-export default App;
